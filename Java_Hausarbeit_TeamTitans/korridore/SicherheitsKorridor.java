c2fdf3f8a8762960563e128a194e1994472a75b6
Patrick <Patrick@10.43.7.4>
2014-05-16 19:33:45 +0200

    Hinzufügung der Methode zur Überprüfung der Orte ob sie zu den Korridorart passen. Mit Exceptionwurf wenn nicht passend.

--- Java_Hausarbeit_TeamTitans/korridore/SicherheitsKorridor.java
+++ Java_Hausarbeit_TeamTitans/korridore/SicherheitsKorridor.java
@@ -1,6 +1,11 @@
 package korridore;
 
+import Exceptions.UngueltigerOrt;
+import orte.Auslandsverbindung;
+import orte.Hauptort;
+import orte.Nebenort;
 import orte.Ort;
+import orte.Umschlagpunkt;
 
 public class SicherheitsKorridor extends Korridor {
 	/* {author=TolleN, HandritschkP} */
@@ -15,14 +20,27 @@ public class SicherheitsKorridor extends Korridor {
 	public static final String BESCHREIBUNG = "Sicherheitskorridor";
 
 	public static final String KENNUNG = "SICH";
+
 	/**
 	 * Konstruktor. Hier ueberpruefung der Orte.
+	 * 
 	 * @param ortA
 	 * @param ortB
 	 */
-	public SicherheitsKorridor(Ort ortA, Ort ortB) {
+
+	/**
+	 * Wirft Exception wenn Orte verwedet wurden, die nicht fuer die Korridorart
+	 * gültig sind. Fehler wird bei der Erzeugung von Korridoren in der Karte
+	 * abgefangen
+	 * 
+	 * @param ortA
+	 * @param ortB
+	 * @throws UngueltigerOrt
+	 */
+	public SicherheitsKorridor(Ort ortA, Ort ortB) throws UngueltigerOrt {
 		super(ortA, ortB);
-		ueberpruefeOrtart();
+		ueberpruefeOrtart(ortA);
+		ueberpruefeOrtart(ortB);
 	}
 
 	public static double getBaukosten() {
@@ -43,8 +61,15 @@ public class SicherheitsKorridor extends Korridor {
 
 	/**
 	 * ueberpruefung, ob ortA, ortB eine Ortsart besitzen (hier hinreichend.)
-	 * (Ort.ortArt)
+	 * (Ort.ortArt) * wirft sonst Exception (UngueltigerOrt) aus
 	 */
-	public void ueberpruefeOrtart() {
+	public void ueberpruefeOrtart(Ort ortA) throws UngueltigerOrt {
+
+		if (ortA.getClass() != Hauptort.class
+				|| ortA.getClass() != Nebenort.class
+				|| ortA.getClass() != Umschlagpunkt.class
+				|| ortA.getClass() != Auslandsverbindung.class) {
+			throw new UngueltigerOrt();
+		}
 	}
 }
