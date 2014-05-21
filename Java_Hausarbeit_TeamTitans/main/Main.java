fb5a8c4f1664ee8ab0653668a226022261dd7270
Patrick <Patrick@10.43.6.220>
2014-05-21 16:03:30 +0200

    Karte ergänzt um 2 Methoden:

--- Java_Hausarbeit_TeamTitans/main/Main.java
+++ Java_Hausarbeit_TeamTitans/main/Main.java
@@ -15,23 +15,18 @@ import dateihandler.KartendateiHandler;
 public class Main {
 	public static void main(String[] args) {
 		// TODO Einzelne Abschnitte gegen Fehler absichern
-		Benutzerinterface gui = new Benutzerinterface();
-		File datei = gui.frageNachKartendatei();
+		
 		Karte de = new Karte();
-		KartendateiHandler verarbeiter = new KartendateiHandler(de, datei);
-		verarbeiter.verarbeiteKartendatei();
-		System.out.println(de.orte);
-		for (Ort b : de.orte){
-			System.out.println(b.name);
-			System.out.println(b.kennung);
-		}
-		de.erzeugeNetz();
-		for (Korridor d : de.eingerichteteKorridore){
-			System.out.println(d.getOrtA());
-			System.out.println(d.getOrtB());
-		}
-		Flugroute ab = new Flugroute(de.orte.get(6),de.orte.get(2),243);
-		ab.ermittleBesteRoute();
-		System.out.println(ab.erzeugeTextausgabeReiseroute());
+		
+//		System.out.println(de.orte);
+//		for (Ort b : de.orte){
+//			System.out.println(b.name);
+//			System.out.println(b.kennung);
+//		}
+		
+		System.out.println(de.berechneNetzMittelpunkt()); 
+		
+		
+		
 	}
 }
