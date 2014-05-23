1db9d075b023587302a55c51c4dcb2ba496bd808
Nils <Nils@>
2014-05-23 20:55:21 +0200

    Einige erste Optimierungen in Karte

--- Java_Hausarbeit_TeamTitans/main/Karte.java
+++ Java_Hausarbeit_TeamTitans/main/Karte.java
@@ -4,12 +4,16 @@ import java.util.ArrayList;
 
 import exceptions.UngueltigerOrt;
 import korridore.Korridor;
+import orte.Auslandsverbindung;
+import orte.Hauptort;
+import orte.Nebenort;
 //import orte.Auslandsverbindung;
 //import orte.Hauptort;
 //import orte.Nebenort;
 import orte.Ort;
 //import orte.Umschlagpunkt;
 //import sun.awt.UngrabEvent;
+import orte.Umschlagpunkt;
 
 /**
  * Die Karte ist
@@ -74,13 +78,227 @@ public class Karte {
 
 	}
 
-	// Methode missverstï¿½ndlich. name geaendert. Vllt variable auch aendern.
 	public ArrayList<Ort> getListeAllerOrte() {
 		return orte;
 	}
 
 	public ArrayList<Korridor> getEingerichteteKorridore() {
 		return eingerichteteKorridore;
+	}
+
+	/**
+	 * Hauptmethode der Klasse Karte. Hier werden alle notwendigen Schritte zur
+	 * Netzerstellung ausgeführt.
+	 */
+	public void erstelleNetz() {
+		verbindeAuslandsorte();
+		//erstelleSternENFC(entferneOrtTyp(Ort.KENNUNG_AUSLANDSVERBINDUNG));
+		//erstelleRingStruktur(entferneOrtTyp(Ort.KENNUNG_AUSLANDSVERBINDUNG));
+		erstelleSternSTND(entferneOrtTyp(Ort.KENNUNG_AUSLANDSVERBINDUNG));
+	}
+
+	public void erstelleRingStruktur(ArrayList<Ort> ohneASL) {
+		try{
+		Integer[] mittelpunkt = berechneNetzMittelpunkt(ohneASL);
+		double distanz = Double.MAX_VALUE;
+		for (Ort ort : ohneASL) {
+			double distanzOrt = Math.sqrt(Math.pow(
+					(mittelpunkt[0] - ort.koordX), 2)
+					+ Math.pow((mittelpunkt[1] - ort.koordY), 2));
+			if (distanzOrt < distanz) {
+				distanz = distanzOrt;
+			}
+		}
+		//Erstellung des Ringes
+		ArrayList<Ort> ringOrte = new ArrayList<Ort>();
+		for (Ort ort : ohneASL) {
+			double distanzOrt = Math.sqrt(Math.pow(
+					(mittelpunkt[0] - ort.koordX), 2)
+					+ Math.pow((mittelpunkt[1] - ort.koordY), 2));
+			if (distanzOrt < (4.0 * distanz)) {
+				ringOrte.add(ort);
+			}
+		}
+		Ort ringstart = ringOrte.get(0);
+		for (int startOrt = 0; startOrt < ringOrte.size(); startOrt++) {
+			if (startOrt != (ringOrte.size() - 1)) {
+				eingerichteteKorridore.add(new Korridor(ringOrte.get(startOrt),
+						ringOrte.get((startOrt + 1)), Korridor.KENNUNG_ENFC));
+			} else {eingerichteteKorridore.add(new Korridor(ringOrte.get(startOrt),ringOrte.get(0), Korridor.KENNUNG_ENFC));}
+		}
+		// Hier erstellung der Liste verbleibenderOrte.
+		ArrayList<Ort> verbleibendeOrte = ohneASL;
+		for (Ort ortA : ringOrte) {
+			verbleibendeOrte.remove(ortA);
+		}
+		for (Ort ortA : verbleibendeOrte){
+		eingerichteteKorridore.add(new Korridor(ortA, findeDichtestenOrtzuDiesem(ortA, ringOrte), Korridor.KENNUNG_ENFC));
+		}
+		} catch (UngueltigerOrt e){}
+	}
+	
+	public Ort findeDichtestenOrtzuDiesem(Ort bezugsOrt, ArrayList<Ort> listeOrte){
+		Ort dichtesterOrt = null;
+		double distanz = Double.MAX_VALUE;
+		for (Ort ort : listeOrte){
+			if (bezugsOrt == ort){
+				continue;
+			}
+			double distanzOrt = Math.sqrt(Math.pow((bezugsOrt.koordX - ort.koordX), 2)+ Math.pow((bezugsOrt.koordY - ort.koordY), 2));
+			if (distanzOrt < distanz){
+				distanz = distanzOrt;
+				dichtesterOrt = ort;
+			}
+		}
+		return dichtesterOrt;
+	}
+
+	public void erstelleSternENFC(ArrayList<Ort> ohneASL) {
+		try {
+			Integer[] mittelpunkt = berechneNetzMittelpunkt(ohneASL);
+			Ort sternMittelpunkt = null;
+			double distanz = Double.MAX_VALUE;
+			for (Ort ort : ohneASL) {
+				double distanzOrt = Math.sqrt(Math.pow(
+						(mittelpunkt[0] - ort.koordX), 2)
+						+ Math.pow((mittelpunkt[1] - ort.koordY), 2));
+				if (distanzOrt < distanz) {
+					distanz = distanzOrt;
+					sternMittelpunkt = ort;
+				}
+			}
+			for (Ort ortA : ohneASL) {
+				if (ortA != sternMittelpunkt) {
+					eingerichteteKorridore.add(new Korridor(sternMittelpunkt,
+							ortA, Korridor.KENNUNG_ENFC));
+				}
+			}
+		} catch (UngueltigerOrt e) {
+		}
+	}
+	
+	public void erstelleSternSTND(ArrayList<Ort> ohneASL) {
+		try {
+			Integer[] mittelpunkt = berechneNetzMittelpunkt(ohneASL);
+			Ort sternMittelpunkt = null;
+			double distanz = Double.MAX_VALUE;
+			for (Ort ort : ohneASL) {
+				double distanzOrt = Math.sqrt(Math.pow(
+						(mittelpunkt[0] - ort.koordX), 2)
+						+ Math.pow((mittelpunkt[1] - ort.koordY), 2));
+				if (distanzOrt < distanz) {
+					distanz = distanzOrt;
+					sternMittelpunkt = ort;
+				}
+			}
+			for (Ort ortA : ohneASL) {
+				if (ortA != sternMittelpunkt) {
+					eingerichteteKorridore.add(new Korridor(sternMittelpunkt,
+							ortA, Korridor.KENNUNG_STND));
+				}
+			}
+		} catch (UngueltigerOrt e) {
+		}
+	}
+
+	public ArrayList<Ort> entferneOrtTyp(String kennung) {
+		ArrayList<Ort> ohneOrte = new ArrayList<Ort>();
+		for (Ort ort : orte) {
+			if (!ort.kennung.equals(kennung)) {
+				ohneOrte.add(ort);
+			}
+		}
+		return ohneOrte;
+	}
+
+	/**
+	 * Diese Methode verbindet alle ASL Orte mit ihrem nächstgelegen Nicht ASL
+	 * Ort.
+	 */
+
+	public void verbindeAuslandsorte() {
+		try {
+			ArrayList<Ort> aslOrte = new ArrayList<Ort>();
+			Ort ortVB = null;
+			for (Ort ortA : orte) {
+				if (ortA.kennung.equals(Ort.KENNUNG_AUSLANDSVERBINDUNG)) {
+					aslOrte.add(ortA);
+				}
+			}
+			for (Ort ortASL : aslOrte) {
+				double distanz = Double.MAX_VALUE;
+				for (Ort ortNOTasl : orte) {
+					if (ortNOTasl.kennung
+							.equals(Ort.KENNUNG_AUSLANDSVERBINDUNG)) {
+						continue;
+					}
+					double distanzVergleich = ermittleOrtsdistanz(ortASL,
+							ortNOTasl);
+					if (distanzVergleich < distanz) {
+						distanz = distanzVergleich;
+						ortVB = ortNOTasl;
+					}
+				}
+				eingerichteteKorridore.add(new Korridor(ortASL, ortVB,
+						Korridor.KENNUNG_SICH));
+			}
+		} catch (UngueltigerOrt e) {
+		}
+	}
+
+	public boolean istEinrichtbarerKorridor(Ort ortA, Ort ortB,
+			String korridorTyp) {
+		if (korridorTyp.equals(Korridor.KENNUNG_ENFC)) {
+			if ((ortA.getKennung().equals(Ort.KENNUNG_HAUPTORT)
+					|| ortA.getKennung().equals(Ort.KENNUNG_NEBENORT) || ortA
+					.getKennung().equals(Ort.KENNUNG_UMSCHLAGPUNKT))
+					&& (ortB.getKennung().equals(Ort.KENNUNG_HAUPTORT)
+							|| ortB.getKennung().equals(Ort.KENNUNG_NEBENORT) || ortB
+							.getKennung().equals(Ort.KENNUNG_UMSCHLAGPUNKT))) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		if (korridorTyp.equals(Korridor.KENNUNG_HLST)) {
+			if ((ortA.getKennung().equals(Ort.KENNUNG_HAUPTORT) || ortA
+					.getKennung().equals(Ort.KENNUNG_UMSCHLAGPUNKT))
+					&& (ortB.getKennung().equals(Ort.KENNUNG_HAUPTORT) || ortB
+							.getKennung().equals(Ort.KENNUNG_UMSCHLAGPUNKT))) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		if (korridorTyp.equals(Korridor.KENNUNG_SICH)) {
+			if ((ortA.getKennung().equals(Ort.KENNUNG_HAUPTORT)
+					|| ortA.getKennung().equals(Ort.KENNUNG_NEBENORT)
+					|| ortA.getKennung().equals(Ort.KENNUNG_UMSCHLAGPUNKT) || ortA
+					.getKennung().equals(Ort.KENNUNG_AUSLANDSVERBINDUNG))
+					&& (ortB.getKennung().equals(Ort.KENNUNG_HAUPTORT)
+							|| ortB.getKennung().equals(Ort.KENNUNG_NEBENORT)
+							|| ortB.getKennung().equals(
+									Ort.KENNUNG_UMSCHLAGPUNKT) || ortB
+							.getKennung()
+							.equals(Ort.KENNUNG_AUSLANDSVERBINDUNG))) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		if (korridorTyp.equals(Korridor.KENNUNG_STND)) {
+			if ((ortA.getKennung().equals(Ort.KENNUNG_HAUPTORT)
+					|| ortA.getKennung().equals(Ort.KENNUNG_NEBENORT) || ortA
+					.getKennung().equals(Ort.KENNUNG_UMSCHLAGPUNKT))
+					&& (ortB.getKennung().equals(Ort.KENNUNG_HAUPTORT)
+							|| ortB.getKennung().equals(Ort.KENNUNG_NEBENORT) || ortB
+							.getKennung().equals(Ort.KENNUNG_UMSCHLAGPUNKT))) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		return false;
 	}
 
 	/**
@@ -99,7 +317,7 @@ public class Karte {
 	}
 
 	/**
-	 * ArrayList orte wird von Dateihandler gefï¿½llt. Pruefe auch ob gleicher
+	 * ArrayList orte wird von Dateihandler gefüllt. Pruefe auch ob gleicher
 	 * hoechster Relevanzgrad vorhanden ist. Dann wird der Ort weitergegeben der
 	 * am naechsten am Mittelpunkt der Karte liegt.
 	 * 
@@ -108,7 +326,7 @@ public class Karte {
 
 	/**
 	 * Bekommt eine Liste mit orten und gibt den mit hoechsten Relevanzgrad
-	 * zurï¿½ck. Ort mit hoechsten Relevanzgrad aber nei Auslandsverbindung.
+	 * zurück. Ort mit hoechsten Relevanzgrad aber nei Auslandsverbindung.
 	 * 
 	 * @param liste
 	 * @return
@@ -138,266 +356,97 @@ public class Karte {
 		}
 		return hoechster;
 	}
-/**
- * Bekommt Liste mit Orten und gibt Liste mit allen Orten, die in der linken Kartenhï¿½lfte liegen zurï¿½ck.
- * @param liste
- * @return
- */
-	public ArrayList<Ort> linkeTeilkarte(ArrayList<Ort> liste) {
-
-		ArrayList<Ort> orteLinks = new ArrayList<>();
-		for (int x = 0; x < orte.size(); x++) {
-			if (orte.get(x).koordX < 99.5
-					&& !(orte.get(x).kennung.equals("ASL"))) {
-				orteLinks.add(orte.get(x));
-			}
-		}
-		return orteLinks;
-	}
 
 	/**
-	 * Bekommt Liste mit allen Orten und gibt Liste mit allen Orten, die in der rechten Kartenhï¿½lfte liegen zurï¿½ck. 
-	 * @param liste
-	 * @return
-	 */
-	public ArrayList<Ort> rechteTeilkarte(ArrayList<Ort> liste) {
-
-		ArrayList<Ort> orteRechts = new ArrayList<>();
-		for (int x = 0; x < orte.size(); x++) {
-			if (orte.get(x).koordX > 99.5
-					&& !(orte.get(x).kennung.equals("ASL"))) {
-				orteRechts.add(orte.get(x));
-			}
-		}
-		return orteRechts;
-	}
-
-	/**
-	 * Bekommt Liste mit Orten und erstellt Sternvariante.
-	 * Erstellt immer alle mï¿½glichen Korridorarten und gibt nur den aus, der die niedrigsten Baukosten hat.
-	 * @param liste
-	 */
-	public void erzeugeKorridor(ArrayList<Ort> liste) {
-
-		ArrayList<Korridor> moeglicheKorridore = new ArrayList<Korridor>();
-
-		for (int i = 0; i < liste.size(); i++) {
-
-			for (int k = 0; k < moeglicheKorridore.size(); k++) {
-				moeglicheKorridore.remove(moeglicheKorridore.size() - 1 - k);
-			}
-
-			if (sucheOrtMitHoechstenRelevanzGrad(liste) != liste.get(i)) {
-
-				try {
-					moeglicheKorridore.add(new Korridor(
-							sucheOrtMitHoechstenRelevanzGrad(liste), liste
-									.get(i), korridore.Korridor.KENNUNG_SICH));
-				} catch (UngueltigerOrt e) {
-				}
-				try {
-					moeglicheKorridore.add(new Korridor(
-							sucheOrtMitHoechstenRelevanzGrad(liste), liste
-									.get(i), korridore.Korridor.KENNUNG_HLST));
-				} catch (UngueltigerOrt e) {
-				}
-				try {
-					moeglicheKorridore.add(new Korridor(
-							sucheOrtMitHoechstenRelevanzGrad(liste), liste
-									.get(i), korridore.Korridor.KENNUNG_ENFC));
-				} catch (UngueltigerOrt e) {
-				}
-				try {
-					moeglicheKorridore.add(new Korridor(
-							sucheOrtMitHoechstenRelevanzGrad(liste), liste
-									.get(i), korridore.Korridor.KENNUNG_STND));
-				} catch (UngueltigerOrt e) {
-				}
-			} else {
-				continue;
-			}
-			System.out.println(moeglicheKorridore);
-			Korridor beste = moeglicheKorridore.get(0);
-
-			for (int j = 1; j < moeglicheKorridore.size(); j++) {
-
-				if (beste.getBaukosten() > moeglicheKorridore.get(j)
-						.getBaukosten()) {
-					beste = moeglicheKorridore.get(j);
-				}
-			}
-			eingerichteteKorridore.add(beste);
-		}
-	}
-
-	/**
-	 * Erzeugt das Netz, indem die Sterne fuer beide Kartenhaelften gebildet werden und die Orte mit hoechsten Relevanzgrad dann verbunden werden.
-	 * Auslandsverbindungen vorher an angebunden mit externen Methode.
-	 */
-	public void erstelleNetz() {
-	//	auslandsverbindungAnsNetzAnbienden();
-		erzeugeKorridor(rechteTeilkarte(orte));
-		erzeugeKorridor(linkeTeilkarte(orte));
-		try {
-			eingerichteteKorridore.add(new Korridor(
-					sucheOrtMitHoechstenRelevanzGrad(linkeTeilkarte(orte)),
-					sucheOrtMitHoechstenRelevanzGrad(rechteTeilkarte(orte)),
-					"HLST"));
-		} catch (UngueltigerOrt e) {
-			try {
-				eingerichteteKorridore
-						.add(new Korridor(
-								sucheOrtMitHoechstenRelevanzGrad(linkeTeilkarte(orte)),
-								sucheOrtMitHoechstenRelevanzGrad(rechteTeilkarte(orte)),
-								"SICH"));
-			} catch (UngueltigerOrt f) {
-				System.out.println("Kann gar nicht passieren!");
-			}
-
-		}
-
-	};
-
-	/**
-	 * Geht Liste der der eingerichteten Korridore durch und summiert die Baukosten und gibt diese aus.
+	 * Geht Liste der der eingerichteten Korridore durch und summiert die
+	 * Baukosten und gibt diese aus.
+	 * 
 	 * @return
 	 */
 
 	public double ermittleGesamteBaukosten() {
 		int gesamtKosten = 0;
-		for (int i = 0; i < eingerichteteKorridore.size(); i++) {
-			gesamtKosten += eingerichteteKorridore.get(i).getBaukosten();
+		for (Korridor korridor : eingerichteteKorridore) {
+			gesamtKosten += korridor.getBaukosten();
 		}
 		return gesamtKosten;
 	}
 
-
-
 	/**
-	 * Verbindet alle Auslandsverbindungen mit dem nï¿½chstliegenden,
-	 * Nichtauslandsverbindungsort mit einen Sicherheitskorridor Fï¿½gt die
-	 * erstllten Korridore der ArrayList eingerichtete Korridore zu.
-	 */
-	
-	public void auslandsverbindungAnsNetzAnbienden() {
-		
-		ArrayList<Ort> listeAuslandsverbindungen = new ArrayList<Ort>();
-		Ort naechsterOrt = orte.get(0);
-
-		for (int i = 1; i < orte.size() && naechsterOrt.kennung.equals("ASL"); i++) {
-			naechsterOrt = orte.get(i);
-		}
-
-		for (int i = 0; i < orte.size(); i++) {
-			
-			if (orte.get(i).kennung.equals("ASL")) {
-				listeAuslandsverbindungen.add(orte.get(i));
-			}
-		}
-	
-			
-			for (int j = 0; j < listeAuslandsverbindungen.size(); j++) {
-
-				for (int k = 0; k < orte.size(); k++) {
-
-					if (!(orte.get(k).kennung.equals("SICH"))) {
-						if (ermittleOrtsdistanz(
-								listeAuslandsverbindungen.get(j), orte.get(k)) < ermittleOrtsdistanz(
-								listeAuslandsverbindungen.get(j), naechsterOrt)) {
-							naechsterOrt = orte.get(k);
-						}
-
-					}
-					
-				}
-				
-				try {
-					eingerichteteKorridore.add(new Korridor(
-							listeAuslandsverbindungen.get(j),
-							naechsterOrt, "SICH"));
-				} catch (UngueltigerOrt e) {
-					continue;
-				}
-				
-			}
-		
-	}
-
-	/**
-	 * Berechnet den Mittelpunt (Punkt, wo die Summe der Abstï¿½nde am kleinsten
-	 * ist) des Netzes. Gibt ArrayList zurï¿½ck. Element0 = X-Koordinate, Element1
+	 * Berechnet den Mittelpunt (Punkt, wo die Summe der Abstände am kleinsten
+	 * ist) des Netzes. Gibt ArrayList zurück. Element0 = X-Koordinate, Element1
 	 * = Y-Koordinate des Mittelpunktes.
 	 * 
 	 * @return
 	 */
 
-	public ArrayList<Integer> berechneNetzMittelpunkt() {
-
-		ArrayList<Integer> punkt = new ArrayList<Integer>();
-		punkt.add(0);
-		punkt.add(0);
-
-		double kleinsteSumme = 222000 * orte.size(); // 222 ist der maximale
-														// Abstand der auf der
-														// Karte
-														// mï¿½glich wï¿½re
-
-		for (int i = 0; i < 200; i++) {
-
-			for (int j = 0; j < 100; j++) {
-
-				Ort fiktivOrt = new Ort(i, j, "fiktivOrt");
-
-				if (summeAbstandermitteln(fiktivOrt) < kleinsteSumme) {
-					kleinsteSumme = summeAbstandermitteln(fiktivOrt);
-					punkt.set(0, i);
-					punkt.set(1, j);
+	public Integer[] berechneNetzMittelpunkt(ArrayList<Ort> vorhandeneOrte) {
+		Integer[] koord = new Integer[2];
+		double distanz = Double.MAX_VALUE;
+		for (int xkoord = 0; xkoord < 200; xkoord++) {
+			for (int ykoord = 0; ykoord < 100; ykoord++) {
+				double gesamtdistanz = 0;
+				for (Ort ort : vorhandeneOrte) {
+					double distanzSchleife = Math.sqrt(Math.pow(
+							(xkoord - ort.koordX), 2)
+							+ Math.pow((ykoord - ort.koordY), 2));
+					gesamtdistanz += distanzSchleife;
+				}
+				if (gesamtdistanz < distanz) {
+					distanz = gesamtdistanz;
+					koord[0] = xkoord;
+					koord[1] = ykoord;
 				}
 			}
 		}
-
-		return punkt;
+		return koord;
 	}
 
-	public double summeAbstandermitteln(Ort orta) {
-		double summeAbstand = 0;
-		for (int i = 0; i < orte.size(); i++) {
-			summeAbstand = summeAbstand
-					+ (1000 * ermittleOrtsdistanz(orta, orte.get(i)));
-		}
-		return summeAbstand;
-	}
-	
 	/**
 	 * uebergabe der Liste Auswertung der Anzahl nach Art (uebergabeparameter
 	 * ist Art des zu analysierenden Korridors)
 	 */
 
 	public int ermittleAnzahlHLSTKorridore() {
-		return ermittleKorridoranzahlNachArt(Korridor.KENNUNG_HLST);
+		int anzahlHLST = 0;
+		for (Korridor korridor : eingerichteteKorridore) {
+			if (korridor.getKennung().equals("HLST")) {
+				anzahlHLST += 1;
+			}
+		}
+		return anzahlHLST;
 	}
 
 	public int ermittleAnzahlSICHKorridore() {
-		return ermittleKorridoranzahlNachArt(Korridor.KENNUNG_SICH);
+		int anzahlSICH = 0;
+		for (Korridor korridor : eingerichteteKorridore) {
+
+			if (korridor.getKennung().equals("SICH")) {
+				anzahlSICH += 1;
+			}
+		}
+		return anzahlSICH;
 	}
 
 	public int ermittleAnzahlENFCKorridore() {
-		return ermittleKorridoranzahlNachArt(Korridor.KENNUNG_ENFC);
+		int anzahlENFC = 0;
+		for (Korridor korridor : eingerichteteKorridore) {
+
+			if (korridor.getKennung().equals("ENFC")) {
+				anzahlENFC += 1;
+			}
+		}
+		return anzahlENFC;
 	}
 
 	public int ermittleAnzahlSTNDKorridore() {
-		return ermittleKorridoranzahlNachArt(Korridor.KENNUNG_STND);
-	}
-	
-	public int ermittleKorridoranzahlNachArt(String kennung){
-		int anzahl = 0;
-		for (int i = 0; i < eingerichteteKorridore.size(); i++) {
+		int anzahlSTND = 0;
+		for (Korridor korridor : eingerichteteKorridore) {
 
-			if (eingerichteteKorridore.get(i).getKennung().equals(kennung)) {
-				anzahl += 1;
+			if (korridor.getKennung().equals("STND")) {
+				anzahlSTND += 1;
 			}
 		}
-		return anzahl;
-		
+		return anzahlSTND;
 	}
 }
