16958437a2ef35789a1418b3d2a7755ae000fb1e
Nils <Nils@>
2014-05-28 19:52:40 +0200

    Exception

---
+++ Java_Hausarbeit_TeamTitans/exceptions/DateiSyntaxFehler.java
@@ -0,0 +1,17 @@
+package exceptions;
+
+public class DateiSyntaxFehler extends Exception{
+	
+	int fehlercode;
+	
+	int zeile;
+
+	public DateiSyntaxFehler(int fehlercode, int zeile) {
+		super();
+		this.fehlercode = fehlercode;
+		this.zeile = zeile;
+	}
+
+	public void zeigeFehlernachricht(){
+	}
+}
