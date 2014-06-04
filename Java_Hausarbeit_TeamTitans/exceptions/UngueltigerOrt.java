7011fe9645d908715166033a75bec48c808528aa
BruecknerR <mail@ruben-brueckner.de>
2014-06-04 11:19:33 +0200

    Hinweise auf Orte und Korridor.

--- Java_Hausarbeit_TeamTitans/exceptions/UngueltigerOrt.java
+++ Java_Hausarbeit_TeamTitans/exceptions/UngueltigerOrt.java
@@ -1,13 +1,23 @@
 package exceptions;
 
+import orte.Ort;
+
 /*Diese Exceptions soll bei Orten geworfen werden, 
 wenn ein unerlaubter Ort bei einem Korridor genutzt wird.
 */
 
-public class UngueltigerOrt extends Exception {
 
-	public UngueltigerOrt() {
-		super("FalscherOrt_benutzt");
+
+public class UngueltigerOrt extends Exception {
+	Ort ortA;
+	Ort ortB;
+	String korridorArt;
+	
+	public UngueltigerOrt(Ort ortA, Ort ortB, String korridorArt) {
+		super("FalscherOrt_benutzt:"+ortA.kennung+", "+ortB.kennung+" sollten fuer einen "+korridorArt+" genutzt werden.");
+		this.ortA = ortA;
+		this.ortB = ortB;
+		this.korridorArt = korridorArt;
 		// TODO Auto-generated constructor stub
 	}
 	
