1dc352def070983570afb39d0e635fd3471cb35a
Nils <Nils@192.168.1.211>
2014-06-07 13:21:02 +0200

    Update Main

--- Java_Hausarbeit_TeamTitans/main/Main.java
+++ Java_Hausarbeit_TeamTitans/main/Main.java
@@ -16,33 +16,34 @@ import exceptions.OrtNichtVorhanden;
 import exceptions.UngueltigerOrt;
 
 /**
- *  Diese Klasse beinhaltet den Gesamtablauf des Simulators inklusive aller Interaktionen mit dem User.
- *  Ermoeglicht das einlesen einer Karte und deren Netzerstellung
- *  Ermoeglicht nur das Abspeichern der Netzdatei.
- *  Ermoeglicht das einlesen und imulieren einr Testdatei mit abspeichern der Ergebnisse.
- *  Ermoeglicht eine Einschaetzung der Netzqualitaet.
- *  @author BruecknerR, FechnerL, HandritschkP, TolleN
+ * Diese Klasse beinhaltet den Gesamtablauf des Simulators inklusive aller
+ * Interaktionen mit dem User. Ermoeglicht das einlesen einer Karte und deren
+ * Netzerstellung Ermoeglicht nur das Abspeichern der Netzdatei. Ermoeglicht das
+ * einlesen und imulieren einr Testdatei mit abspeichern der Ergebnisse.
+ * Ermoeglicht eine Einschaetzung der Netzqualitaet.
+ * 
+ * @author BruecknerR, FechnerL, HandritschkP, TolleN
  */
 public class Main {
 	public static void main(String[] args) {
 		boolean dateiLesenErfolgreich = true;
-		//Erzeugung des Benutzerinterfaces.
+		// Erzeugung des Benutzerinterfaces.
 		Benutzerinterface gui = new Benutzerinterface();
-		//Erzeugung einer Karte.
+		// Erzeugung einer Karte.
 		Karte karte = new Karte();
-		//Erzeugung eines Simulators.
+		// Erzeugung eines Simulators.
 		Simulator sim = new Simulator();
-		//Erzeugung eines Simulationsdateiverarbeiter.
+		// Erzeugung eines Simulationsdateiverarbeiter.
 		SimulationsdateiHandler simSchreiber = new SimulationsdateiHandler(sim);
-		//User wird begruesst.
+		// User wird begruesst.
 		gui.begruessung();
-		//Frage nach dem Budget
+		// Frage nach dem Budget
 		double budget = gui.abfrageBudget();
 		karte.setBudget(budget);
 		/*
-		 * Fragen nach einer gueltigen Kartendatei, die dem Format entspricht. 
-		 * Dies wird solange getan bis der User es abbricht 
-		 * oder eine korrekte Datei eingelesen wurde.
+		 * Fragen nach einer gueltigen Kartendatei, die dem Format entspricht.
+		 * Dies wird solange getan bis der User es abbricht oder eine korrekte
+		 * Datei eingelesen wurde.
 		 */
 		do {
 			try {
@@ -56,13 +57,13 @@ public class Main {
 				dateiLesenErfolgreich = false;
 			}
 		} while (!dateiLesenErfolgreich);
-		//Erstellung eines Netzes anhand der Karte und Budget.
+		// Erstellung eines Netzes anhand der Karte und Budget.
 		karte.erstelleNetz();
-		//Meldung über die Baukosten.
+		// Meldung über die Baukosten.
 		gui.zeigeBaukosten(karte);
-		//Frage ob nur das Netz gespeichert werden soll.
+		// Frage ob nur das Netz gespeichert werden soll.
 		int entscheidungNetzspeichern = gui.abfrageNetzSpeichern();
-		if (entscheidungNetzspeichern == 0){
+		if (entscheidungNetzspeichern == 0) {
 			NetzdateiHandler kartenSchreiber = new NetzdateiHandler(karte);
 			kartenSchreiber.schreibeNetzdatei();
 			System.exit(0);
@@ -80,30 +81,32 @@ public class Main {
 				dateiLesenErfolgreich = false;
 			}
 		} while (!dateiLesenErfolgreich);
-		//Simulation der eingelesen Flugrouten über das eben erstellte Netz.
+		// Simulation der eingelesen Flugrouten über das eben erstellte Netz.
 		sim.simuliere();
-		//Anzeigen der Nutzkosten, inklusive der Budgetueberschreitung.
+		// Anzeigen der Nutzkosten, inklusive der Budgetueberschreitung.
 		gui.zeigeNutzkosten(sim, karte);
-		//Fragen des User, wie weiter verfahren werden soll. Details in Methode "frageNachEndoption".
+		// Fragen des User, wie weiter verfahren werden soll. Details in Methode
+		// "frageNachEndoption".
 		int endOption = gui.frageNachEndoption();
-		//Speichern von Netz und Simulation, frage nach weiterer Testdatei.
+		// Speichern von Netz und Simulation, frage nach weiterer Testdatei.
 		if (endOption == 1) {
 			NetzdateiHandler kartenSchreiber = new NetzdateiHandler(karte);
 			kartenSchreiber.schreibeNetzdatei();
 			simSchreiber.schreibeSimulationsDatei();
+			//Leeren der Liste eingelesener Flugrouten um neue Flugrouten aufzunehmen.
 			sim.routen.clear();
-		//Speichern von beiden Dateien und Beendigung des Programms.
+			// Speichern von beiden Dateien und Beendigung des Programms.
 		} else if (endOption == 0) {
 			NetzdateiHandler kartenSchreiber = new NetzdateiHandler(karte);
 			kartenSchreiber.schreibeNetzdatei();
 			simSchreiber.schreibeSimulationsDatei();
-			sim.routen.clear();
 			System.exit(0);
-		//Programm wird nur beendet.
+			// Programm wird nur beendet.
 		} else if (endOption == 2) {
 			System.exit(0);
 		}
-		//Hier geht es nach EndOption = 1 weiter, mit der erneuten Frage nach einer Testdatei.
+		// Hier geht es nach EndOption = 1 weiter, mit der erneuten Frage nach
+		// einer Testdatei.
 		while (endOption == 1) {
 			do {
 				try {
@@ -116,12 +119,14 @@ public class Main {
 					sim.routen.clear();
 					dateiLesenErfolgreich = false;
 				}
-			//Schleife ermoeglicht das wiederholte Einlesen und Simulieren von Testdateien.
+				// Schleife ermoeglicht das wiederholte Einlesen und Simulieren
+				// von Testdateien.
 			} while (!dateiLesenErfolgreich);
-			//Erneute Simulation und Anzeigen der Nutzkosten.
+			// Erneute Simulation und Anzeigen der Nutzkosten.
 			sim.simuliere();
 			gui.zeigeNutzkosten(sim, karte);
-			//Hier folgt die erneute Frage, wie der User weiter verfahren moechte.
+			// Hier folgt die erneute Frage, wie der User weiter verfahren
+			// moechte.
 			endOption = gui.frageNachEndoption();
 			if (endOption == 0) {
 				simSchreiber.schreibeSimulationsDatei();
