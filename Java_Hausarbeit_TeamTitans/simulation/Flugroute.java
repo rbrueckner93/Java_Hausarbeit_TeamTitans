2ca525634d7fd8156c59c2c8664af6ca0c3a6719
BruecknerR <mail@ruben-brueckner.de>
2014-06-10 00:15:10 +0200

    Kuerzungen. Verbesserungen. Kommentare.

--- Java_Hausarbeit_TeamTitans/simulation/Flugroute.java
+++ Java_Hausarbeit_TeamTitans/simulation/Flugroute.java
@@ -9,28 +9,35 @@ import exceptions.OrtNichtVorhanden;
 import orte.Ort;
 
 /**
- * Wird erst bei Nutzung, dem Testdateieinlesen erstellt mit Start, Ziel und
- * Faktor. Sucht sich nach Aufforderung durch ermittleBesteRoute (nicht schon
- * bei Initialisierung) ihren eigenen optimalen Weg, und schreibt diesen in der
- * reiseListe fest und stellt die Nutzkosten in ermittleRoutennutzkosten fest.
+ * Wird erst bei Nutzung, dem Testdateieinlesen mit Start, Ziel und Faktor
+ * erstellt. Sucht sich erst nach Aufforderung durch ermittleBesteRoute ihren
+ * eigenen optimalen Weg, und schreibt diesen in der reiseListe fest und stellt
+ * die Nutzkosten in ermittleRoutennutzkosten fest.
  * 
  * @author BruecknerR
  */
 public class Flugroute {
 	private Ort ziel;
 	private Ort herkunft;
-	// ArrayList mit Flurkorridoren, die mit einer Flugroute genutzt werden.
+	/**
+	 * ArrayList mit Korridoren, die mit einer Flugroute genutzt werden.
+	 */
 	private ArrayList<Korridor> reiseListe;
-	// Haeufigkeit der Fluege auf Route
+	/**
+	 * Haeufigkeit der Fluege auf der Route.
+	 */
 	private int faktor;
 
 	/**
 	 * Konstruktor der Klasse Flugroute. Parameter werden von TestdateiHanlder
 	 * uebergeben.
 	 * 
-	 * @param ziel Ort des Ziels
-	 * @param herkunft Ort der Herkunft
-	 * @param faktor Haeufigkeit der Nutzung
+	 * @param ziel
+	 *            Ort des Ziels
+	 * @param herkunft
+	 *            Ort der Herkunft
+	 * @param faktor
+	 *            Haeufigkeit der Fluege auf der Route.
 	 */
 	public Flugroute(Ort ziel, Ort herkunft, int faktor) {
 		super();
@@ -44,42 +51,26 @@ public class Flugroute {
 		return ziel;
 	}
 
-	public void setZiel(Ort ziel) {
-		this.ziel = ziel;
-	}
-
 	public Ort getHerkunft() {
 		return herkunft;
-	}
-
-	public void setHerkunft(Ort herkunft) {
-		this.herkunft = herkunft;
 	}
 
 	public ArrayList<Korridor> getReiseListe() {
 		return reiseListe;
 	}
 
-	public void setReiseListe(ArrayList<Korridor> reiseListe) {
-		this.reiseListe = reiseListe;
+	public void addToReiseListe(Korridor korridor) {
+		this.reiseListe.add(korridor);
 	}
 
 	public int getFaktor() {
 		return faktor;
 	}
 
-	public void setFaktor(int faktor) {
-		this.faktor = faktor;
-	}
-
 	/**
-	 * besteRoute wird auf einer Flugroute ausgefuehrt. Sie dient dazu, den
-	 * besten Weg von A nach B zu finden, wobei lediglich die entstehenden
-	 * Nutzungskosten zugrunde gelegt werden.
-	 * 
-	 * Beste Route anhand unterschiedlicher Korridorkombinationen ermitteln.
+	 * BesteRoute wird auf einer Flugroute ausgefuehrt. Sie dient dazu, den
+	 * kostenguenstigsten Weg von A nach B zu finden.
 	 */
-
 	public void ermittleBesteRoute() {
 		/*
 		 * flugroutenInArbeit speichert nur solange Elemente, wie sie nicht am
@@ -92,15 +83,16 @@ public class Flugroute {
 		 */
 		ArrayList<Flugroute> moeglicheFlugrouten = new ArrayList<Flugroute>();
 
-		// von Start ausgehend alle angebundenen Korridore als
-		// flugroutenInArbeit anlegen.
+		// Vom Start ausgehend alle angebundenen Korridore als erste
+		// Listenelemente der reiseListe von flugroutenInArbeit anlegen.
 		for (int i = 0; i < herkunft.getAngebundeneKorridore().size(); i++) {
-			// ueberpruefen, ob mit dem ersten Hop bereits das Ziel erreicht
+			// Ueberpruefen, ob mit dem ersten Hop bereits das Ziel erreicht
 			// wurde, wenn ja als moeglicheFlugroute abspeichern.
 			Ort erstesZiel = herkunft.getAngebundeneKorridore().get(i)
 					.bestimmeAnderenOrt(herkunft);
 			Flugroute ersteRoute = new Flugroute(erstesZiel, herkunft, faktor);
-			ersteRoute.reiseListe.add(herkunft.getAngebundeneKorridore().get(i));
+			ersteRoute.reiseListe
+					.add(herkunft.getAngebundeneKorridore().get(i));
 			if (erstesZiel == ziel) {
 				moeglicheFlugrouten.add(ersteRoute);
 			}
@@ -112,11 +104,13 @@ public class Flugroute {
 		}
 
 		// solange in der "In Arbeit"-Liste noch Flugrouten stehen:
-		while (flugroutenInArbeit.size() > 0) {
+		while (!flugroutenInArbeit.isEmpty()) {
 			for (int routen = 0; routen < flugroutenInArbeit.size(); routen++) {
 				Flugroute weg = flugroutenInArbeit.get(routen);
-				for (int i = 0; i < weg.ziel.getAngebundeneKorridore().size(); i++) {
-					Korridor verbindung = weg.ziel.getAngebundeneKorridore().get(i);
+				for (int i = 0; i < weg.getZiel().getAngebundeneKorridore()
+						.size(); i++) {
+					Korridor verbindung = weg.getZiel()
+							.getAngebundeneKorridore().get(i);
 					/*
 					 * wenn ermittleAnderenOrt(verbindung, weg.ziel) nicht in
 					 * weg.erzeugeOrtsListe vorkommt und
@@ -125,15 +119,14 @@ public class Flugroute {
 					 */
 					Ort neuesZiel = verbindung.bestimmeAnderenOrt(weg.ziel);
 					try {
-						if (weg.erzeugeOrtsListe().size() > 0) {
-							if (!kommtOrtInOrtslisteVor(neuesZiel,
-									weg.erzeugeOrtsListe())) {
+						if (!weg.erzeugeOrtsListe().isEmpty()) {
+							if (!weg.erzeugeOrtsListe().contains(neuesZiel)) {
 								Flugroute neueFlugroute = new Flugroute(
 										neuesZiel, herkunft, faktor);
 								for (Korridor add : weg.reiseListe) {
-									neueFlugroute.reiseListe.add(add);
+									neueFlugroute.addToReiseListe(add);
 								}
-								neueFlugroute.reiseListe.add(verbindung);
+								neueFlugroute.addToReiseListe(verbindung);
 								if (neuesZiel != ziel) {
 									flugroutenInArbeit.add(neueFlugroute);
 								}
@@ -167,7 +160,7 @@ public class Flugroute {
 		}
 
 		/*
-		 * Nimm die guenstigste Flugroute und gib sie zurueck.
+		 * Ermittlung der guenstigste Flugroute unter den moeglichen Flugrouten.
 		 */
 		while (moeglicheFlugrouten.size() > 1) {
 			for (int i = 0; i < moeglicheFlugrouten.size(); i++) {
@@ -181,9 +174,8 @@ public class Flugroute {
 
 		}
 
-		if (moeglicheFlugrouten.size() == 1) {
-			reiseListe = moeglicheFlugrouten.get(0).reiseListe;
-		}
+		if (moeglicheFlugrouten.size() == 1)
+			reiseListe.addAll(moeglicheFlugrouten.get(0).reiseListe);
 	}
 
 	/**
@@ -193,8 +185,9 @@ public class Flugroute {
 	 * Die einmaligen Nutzungskosten muessen nicht separat ermittelt werden, da
 	 * sowohl bei der Wahl der Korridore als auch der Ermittlung der
 	 * Gesamtkosten nur die Gesamtkosten der Nutzung einer Route von Bedeutung
-	 * sind. Dabei werden korridor.ermittleLaenge(); und korridor.NutzungskostenProKm
-	 * herangezogen.
+	 * sind. Dabei werden korridor.ermittleLaenge(); und
+	 * korridor.NutzungskostenProKm herangezogen.
+	 * 
 	 * @return Kosten fuer alle Durchfluege
 	 */
 
@@ -202,14 +195,16 @@ public class Flugroute {
 		double kosten = 0.0;
 		for (Korridor relation : reiseListe) {
 			kosten = kosten
-					+ ((relation.getNutzungskostenProKm() * relation.getLaenge()) * faktor);
+					+ ((relation.getNutzungskostenProKm() * relation
+							.getLaenge()) * faktor);
 		}
 		return kosten;
 	}
 
 	/**
-	 * Methode wertet die Reiseliste einer Flugroute aus und schreibt 
-	 * die Ortsnamen mit ";" getrennt in eine String
+	 * Methode wertet die Reiseliste einer Flugroute aus und schreibt die
+	 * Ortsnamen mit ";" getrennt in eine String
+	 * 
 	 * @author BruecknerR
 	 * @return String mit Semikolon-getrennten Ortsnamen wird von Dateihandler
 	 *         verwendet
@@ -223,7 +218,7 @@ public class Flugroute {
 			ausgabe = ausgabe.substring(0, ausgabe.length() - 1);
 		} catch (OrtNichtVorhanden e) {
 			JOptionPane.showMessageDialog(null,
-					"Achtung, Programm jetzt beendet.");
+					"Achtung, Programm beendet sich aufgrund eines Fehlers.");
 			System.exit(0);
 		}
 		return ausgabe;
@@ -234,9 +229,11 @@ public class Flugroute {
 	 * ausgibt. Aus der ArrayList<Korridor> mit ortA und ortB muss also nach dem
 	 * Ort des ersten Korridors gesucht werden, der Herkunft entspricht. Im
 	 * zweiten Korridor wird festgestellt, welcher Ort nicht dem zweiten Ort des
-	 * ersten Korridors entspricht usw., so dass der letzte Ort in der reiseliste
-	 * dem Ziel entspricht. Eine Ueberpruefung muss natuerlich stattfinden. Sie
-	 * ist eine direkte Ableitung aus reiseListe ArrayList<Korridor>
+	 * ersten Korridors entspricht usw., so dass der letzte Ort in der
+	 * reiseliste dem Ziel entspricht. Eine Ueberpruefung muss natuerlich
+	 * stattfinden. Sie ist eine direkte Ableitung aus reiseListe
+	 * ArrayList<Korridor>
+	 * 
 	 * @return Liste aller Orte die Durchflogen wurden
 	 */
 	private ArrayList<Ort> erzeugeOrtsListe() throws OrtNichtVorhanden {
@@ -246,8 +243,8 @@ public class Flugroute {
 			for (Korridor verbindung : reiseListe) {
 				if (verbindung.getOrtA() == ortsListe.get(ortsListe.size() - 1)) {
 					ortsListe.add(verbindung.getOrtB());
-				} else if (verbindung.getOrtB() == ortsListe
-						.get(ortsListe.size() - 1)) {
+				} else if (verbindung.getOrtB() == ortsListe.get(ortsListe
+						.size() - 1)) {
 					ortsListe.add(verbindung.getOrtA());
 				} else {
 					throw new OrtNichtVorhanden();
@@ -255,25 +252,5 @@ public class Flugroute {
 			}
 		}
 		return ortsListe;
-	}
-
-	/**
-	 * Ueberprueft eine ArrayList von Orten auf Vorkommen eines bestimmten
-	 * Ortes.
-	 * 
-	 * @param ort
-	 *            : Ein beliebiger Ort
-	 * @param ortsliste
-	 *            : Eine ArrayList mit Orten
-	 * @return true: ort kommt in der ortsliste vor.
-	 * @author: BruecknerR
-	 */
-	private boolean kommtOrtInOrtslisteVor(Ort ort, ArrayList<Ort> ortsliste) {
-		for (Ort testort : ortsliste) {
-			if (testort == ort) {
-				return true;
-			}
-		}
-		return false;
 	}
 }
